72db7c4420bd8ed0bf4490ea63e21ebd6d4a37e7
Mario23junior <mariojunior3251@gmail.com>
2021-12-19 04:40:13 -0300

    Iniciando projeto de missaoes realizadas para os estudos do planeta jupiter

---
+++ src/main/java/com/project/missaojupiter/MissaoJupiterApplication.java
@@ -0,0 +1,13 @@
+package com.project.missaojupiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissaoJupiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissaoJupiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/missaojupiter/MissaoJupiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.missaojupiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissaoJupiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
