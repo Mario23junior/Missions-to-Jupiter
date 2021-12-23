ca723e010e10356f7a6501af2cb0f0c7a91366fe
Mario23junior <mariojunior3251@gmail.com>
2021-12-23 02:58:50 -0300

    Alterando modularidade das pastas de model

---
+++ src/main/java/com/project/missaojupiter/model/Galileo/Missao.java
@@ -0,0 +1,52 @@
+package com.project.missaojupiter.model.Galileo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Missao {
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date dataDeLacamento;
+	private String destino;
+	private Date dataDeAterrissagem;
+	private String veiculoDeLancamento;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getDataDeLacamento() {
+		return dataDeLacamento;
+	}
+	public void setDataDeLacamento(Date dataDeLacamento) {
+		this.dataDeLacamento = dataDeLacamento;
+	}
+	public String getDestino() {
+		return destino;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	public Date getDataDeAterrissagem() {
+		return dataDeAterrissagem;
+	}
+	public void setDataDeAterrissagem(Date dataDeAterrissagem) {
+		this.dataDeAterrissagem = dataDeAterrissagem;
+	}
+	public String getVeiculoDeLancamento() {
+		return veiculoDeLancamento;
+	}
+	public void setVeiculoDeLancamento(String veiculoDeLancamento) {
+		this.veiculoDeLancamento = veiculoDeLancamento;
+	}
+	
+	
+}
