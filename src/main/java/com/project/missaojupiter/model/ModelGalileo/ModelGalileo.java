c59ec3e424eb997bc8141eb64b2852f51e4fa46a
Mario23junior <mariojunior3251@gmail.com>
2021-12-23 02:30:59 -0300

    Criando tabela de informações sobre a sonda

---
+++ src/main/java/com/project/missaojupiter/model/ModelGalileo/ModelGalileo.java
@@ -0,0 +1,59 @@
+package com.project.missaojupiter.model.ModelGalileo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Galileo")
+ public class ModelGalileo {
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private String descricao;
+	private String indentificacao;
+	private String duracaoDaMiscao;
+	
+	public ModelGalileo() {
+ 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getIndentificacao() {
+		return indentificacao;
+	}
+
+	public void setIndentificacao(String indentificacao) {
+		this.indentificacao = indentificacao;
+	}
+
+	public String getDuracaoDaMiscao() {
+		return duracaoDaMiscao;
+	}
+
+	public void setDuracaoDaMiscao(String duracaoDaMiscao) {
+		this.duracaoDaMiscao = duracaoDaMiscao;
+	}
+	
+	
+}
