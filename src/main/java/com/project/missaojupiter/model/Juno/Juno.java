e40af3f44df59254ee5e0d5065a604b06c98d658
Mario23junior <mariojunior3251@gmail.com>
2021-12-23 22:57:42 -0300

    Criando tabela de informcações

--- src/main/java/com/project/missaojupiter/model/Juno/Juno.java
+++ src/main/java/com/project/missaojupiter/model/Juno/Juno.java
@@ -1,6 +1,65 @@
 package com.project.missaojupiter.model.Juno;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Juno {
-   
+  
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String tipo;
+	private Double duracao;
+	
+	public Juno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Double getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(Double duracao) {
+		this.duracao = duracao;
+	}
+	
+	
 	
 }
