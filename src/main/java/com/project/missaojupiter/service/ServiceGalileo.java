3c707fad712ba3934a4fa3ae4a87df22f14f14c9
Mario23junior <mariojunior3251@gmail.com>
2021-12-26 02:32:22 -0300

    Criando listagem por id da sonda galileo

--- src/main/java/com/project/missaojupiter/service/ServiceGalileo.java
+++ src/main/java/com/project/missaojupiter/service/ServiceGalileo.java
@@ -1,9 +1,12 @@
 package com.project.missaojupiter.service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.project.missaojupiter.ModelDTO.GalileoDto.GalileoDto;
 import com.project.missaojupiter.model.Galileo.Galileo;
@@ -31,4 +34,15 @@ public class ServiceGalileo {
 	   Galileo save = galileoRepository.save(galileo);
 	   return save;
 	}
+	
+	public ResponseEntity<GalileoDto> listId(@PathVariable Long id) {
+		Optional<Galileo> listId = galileoRepository.findById(id);
+		if(listId.isPresent()) {
+			return ResponseEntity.ok(mapper.map(listId.get(), GalileoDto.class));
+		} else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	
 }
