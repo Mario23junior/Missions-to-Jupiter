4e8e8745c63c6d14a342729901b33958224e3df6
Mario23junior <mariojunior3251@gmail.com>
2021-12-26 02:56:15 -0300

    Criando informacoes da sonda

--- src/main/java/com/project/missaojupiter/controller/ControllerGalileo.java
+++ src/main/java/com/project/missaojupiter/controller/ControllerGalileo.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +31,22 @@ public class ControllerGalileo {
 	public ResponseEntity<GalileoDto> list(@PathVariable Long id){
 		return service.listId(id);
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<GalileoDto> update(@PathVariable Long id, @RequestBody GalileoDto galileoDto) {
+		return service.updateIdInfo(id, galileoDto);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
