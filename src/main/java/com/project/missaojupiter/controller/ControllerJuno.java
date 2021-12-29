b44bc60d234499172905059f8b4a3366ef8cfc15
Mario23junior <mariojunior3251@gmail.com>
2021-12-29 00:47:13 -0300

    Listando informacoes por codigo de idenficacao

--- src/main/java/com/project/missaojupiter/controller/ControllerJuno.java
+++ src/main/java/com/project/missaojupiter/controller/ControllerJuno.java
@@ -1,6 +1,8 @@
 package com.project.missaojupiter.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,10 @@ public class ControllerJuno {
 	@PostMapping
 	public ResponseEntity<JunoDto> saveJuno(@RequestBody JunoDto juoDto) {
 		return service.save(juoDto);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<JunoDto> listId(@PathVariable Long id) {
+		return service.listId(id);
 	}
 }
