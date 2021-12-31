57d0a096ef7b35be9d447f26416989cc0783239d
Mario23junior <mariojunior3251@gmail.com>
2021-12-31 03:19:06 -0300

    deletando informacoes por id

--- src/main/java/com/project/missaojupiter/controller/ControllerJuno.java
+++ src/main/java/com/project/missaojupiter/controller/ControllerJuno.java
@@ -1,6 +1,7 @@
 package com.project.missaojupiter.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +36,10 @@ public class ControllerJuno {
 	@PutMapping("{id}")
 	public ResponseEntity<JunoDto> update(@PathVariable Long id , @RequestBody JunoDto junoDto) {
 		return service.updateData(id, junoDto);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<JunoDto> deleteData(@PathVariable Long id) {
+		return service.delete(id);
 	}
 }
